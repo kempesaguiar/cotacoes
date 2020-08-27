9a75e3afc70671dc203e20836cb29dd9e4214460
kempesaguiar <kempesaguiar@gmail.com>
2020-08-27 08:53:00 -0300

    Ajustes no Repository

--- src/main/java/com/kempes/cotacoes/repository/CotacaoRepository.java
+++ src/main/java/com/kempes/cotacoes/repository/CotacaoRepository.java
@@ -1,9 +1,13 @@
 package com.kempes.cotacoes.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.kempes.cotacoes.models.Cotacao;
 
 public interface CotacaoRepository extends CrudRepository<Cotacao, Long> {
+	
+	@Query(value = "Select count(*) from tb_cotacao where data_cotacao like ?1", nativeQuery = true)
+	long findTotalByData(String data_cotacao);
 
 }
